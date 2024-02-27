40e823d6a1e8d3a9e75cb4d2614e709dfdf72b96
Sonu Chaurasia <sonu.chaurasia@carelon.com>
2024-02-27 18:57:39 +0530

    skelaton project structure

---
+++ src/main/java/com/sonu/recipeinsta/RecipeInstaApplication.java
@@ -0,0 +1,14 @@
+package com.sonu.recipeinsta;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeInstaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeInstaApplication.class, args);
+	}
+
+}
