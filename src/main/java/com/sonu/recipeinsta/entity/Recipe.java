8e4c9350df4d3479b7a05227ad50f25ff5614e51
Sonu chaurasia <sonu.chaurasia@citiustech.com>
2024-03-01 21:50:18 +0530

    user entity added and mysql configuration

--- src/main/java/com/sonu/recipeinsta/entity/Recipe.java
+++ src/main/java/com/sonu/recipeinsta/entity/Recipe.java
@@ -1,4 +1,12 @@
 package com.sonu.recipeinsta.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+//@Entity
 public class Recipe {
+//    @Id
+//    @GeneratedValue()
+//    private int id;
 }
